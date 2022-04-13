2c07ce02682aebeafd0bb3b932ed0d98f85926df
Henrique Barreto Leonardo <henriquelb28@gmail.com>
2022-04-13 17:08:48 -0300

    Cadastro com o cognito concluido

--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -1,41 +1,73 @@
-import React, {useState, useEffect} from "react"
+import React, {useState} from "react"
 import UserPool from "../Utils/UserPool";
-import {CognitoUser} from 'amazon-cognito-identity-js';
-
-
+// import {CognitoUser} from 'amazon-cognito-identity-js';
+import S3FileUpload from 'react-s3';
 
 export default function Cadastrar() {
     const [email, setEmail] = useState("")
     const [senha, setSenha] = useState("")
     const [nomeC, setNomeC] = useState("")
+    const [Access, setAccess] = useState("")
+    const [Private, setPrivate] = useState("")
+    // const [Pfp, setPfp] = useState("")
     
     
+    const CreateBucket = (e) =>{
+        
+            const config = {
+                bucketName: "A" + nomeC + Math.floor((Math.random() * 1000) + 1),
+                dirName: 'profilePhoto', 
+                region: 'us-east-1',
+                accessKeyId: 'AKIA6FJTISO6RD4ZGMK7',
+                secretAccessKey: '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',
+            }
+
+            S3FileUpload.uploadFile(e.target.files[0], config)
+            .then((data) =>{
+                console.log(data)
+            })
+            .catch((err)=> {
+                console.log(err)
+            })
+    
+        
+    }
+    
+    
+     
     
     
 
     //{name: nomeC},{Access_Key: "12345"},{Secret_Key: "123456"}    
 
-    Cadastrar = (event) =>{
+    const Cadastrar = (event) =>{
         event.preventDefault()
 
         console.log(email)
         console.log(senha)
         console.log(nomeC)
+        
 
-        // AtributeList = [
-        //     {
-        //         Name: 'name',
-        //         value: nomeC
-        //     },
-        //     {
-        //         Name: 'Access_Key',
-        //         Value: 
-        //     }
-        // ]
+        var AtributeList = [
+            {
+                Name: 'name',
+                Value: nomeC
+            },
+            {
+                Name: 'custom:Access_Key',
+                Value: Access
+            },
+            {
+                Name: 'custom:Private_Key',
+                Value: Private
+            }
+        ]
+
+        // {Name: 'name', Value: nomeC},{Name: 'custom:Access_Key',Value: Access},{Name: 'custom:Private_Key',Value: Private}
 
         
         
-        UserPool.signUp(email, senha,  [{Name: 'name',Value: nomeC }], null, (err, data) => {
+        UserPool.signUp(email, senha, [AtributeList], null, (err, data) => {
             if (err) {
                 console.error(err)
             }
@@ -54,9 +86,15 @@ export default function Cadastrar() {
             <input type="email" value={email} id="Email" onChange={(e) => setEmail(e.target.value)} />
             <label htmlFor="Senha">Senha</label>
             <input type="text" value={senha} id="Senha" onChange={(e) => setSenha(e.target.value)} />
+            <label htmlFor="AccessKey">Access Key</label>
+            <input type="text" value={Access} id="AccessKey" onChange={(e) => setAccess(e.target.value)} />
+            <label htmlFor="PrivateKey">Private Key</label>
+            <input type="text" value={Private} id="PrivateKey" onChange={(e) => setPrivate(e.target.value)} />
+
             <button type="submit">Enviar</button>
 
         </form>
+            <input type="file"  id="PrivateKey" onChange={CreateBucket} />
         </>
     )
 }
--- src/Utils/UserPool.jsx
+++ src/Utils/UserPool.jsx
@@ -2,8 +2,8 @@ import {CognitoUserPool} from 'amazon-cognito-identity-js';
 
 
  const Pooldata = {
-    UserPoolId: "us-east-1_pypmBYsh8",
-    ClientId: "usa573bocubjadq9kcqa2m0ni"
+    UserPoolId: "us-east-1_6v2awSjTC",
+    ClientId: "1132s6s4sjg8hjpb8unvpkq664"
 } 
 
 export default new CognitoUserPool(Pooldata);
