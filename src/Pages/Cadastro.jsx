b7688617202b7f4ce1283e209b432e2313cdb62d
Henrique Barreto Leonardo <henriquelb28@gmail.com>
2022-04-14 17:06:16 -0300

    Tela de cadastro

--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -1,7 +1,11 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 import UserPool from "../Utils/UserPool";
+import "../Assets/Css/CadastroStyle.css";
 // import {CognitoUser} from 'amazon-cognito-identity-js';
-import S3FileUpload from 'react-s3';
+// import S3FileUpload from 'react-s3';
+// import Amplify from "@aws-amplify/core";
+// import { Storage } from "aws-amplify";
+// import { Auth } from 'aws-amplify';
 
 export default function Cadastrar() {
     const [email, setEmail] = useState("")
@@ -9,44 +13,17 @@ export default function Cadastrar() {
     const [nomeC, setNomeC] = useState("")
     const [Access, setAccess] = useState("")
     const [Private, setPrivate] = useState("")
-    // const [Pfp, setPfp] = useState("")
-    
-    
-    const CreateBucket = (e) =>{
-        
-            const config = {
-                bucketName: "A" + nomeC + Math.floor((Math.random() * 1000) + 1),
-                dirName: 'profilePhoto', 
-                region: 'us-east-1',
-                accessKeyId: 'AKIA6FJTISO6RD4ZGMK7',
-                secretAccessKey: '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',
-            }
 
-            S3FileUpload.uploadFile(e.target.files[0], config)
-            .then((data) =>{
-                console.log(data)
-            })
-            .catch((err)=> {
-                console.log(err)
-            })
-    
-        
-    }
-    
-    
-     
-    
-    
 
-    //{name: nomeC},{Access_Key: "12345"},{Secret_Key: "123456"}    
 
-    const Cadastrar = (event) =>{
+
+    const Cadastrar = (event) => {
         event.preventDefault()
 
         console.log(email)
         console.log(senha)
         console.log(nomeC)
-        
+
 
         var AtributeList = [
             {
@@ -63,38 +40,60 @@ export default function Cadastrar() {
             }
         ]
 
-        // {Name: 'name', Value: nomeC},{Name: 'custom:Access_Key',Value: Access},{Name: 'custom:Private_Key',Value: Private}
 
-        
-        
-        UserPool.signUp(email, senha, [AtributeList], null, (err, data) => {
+        UserPool.signUp(email, senha, [{
+            Name: 'name',
+            Value: nomeC
+        },
+        {
+            Name: 'custom:Access_Key',
+            Value: Access
+        },
+        {
+            Name: 'custom:Private_Key',
+            Value: Private
+        }], null, (err, data) => {
             if (err) {
                 console.error(err)
             }
             console.log(data)
         })
+
+
     }
 
 
 
-    return(
+    return (
         <>
-        <form onSubmit={Cadastrar}>
-            <label htmlFor="Name">Nome Completo</label>
-            <input type="text" value={nomeC} id="Name" onChange={(e) => setNomeC(e.target.value)} />
-            <label htmlFor="Email">Email</label>
-            <input type="email" value={email} id="Email" onChange={(e) => setEmail(e.target.value)} />
-            <label htmlFor="Senha">Senha</label>
-            <input type="text" value={senha} id="Senha" onChange={(e) => setSenha(e.target.value)} />
-            <label htmlFor="AccessKey">Access Key</label>
-            <input type="text" value={Access} id="AccessKey" onChange={(e) => setAccess(e.target.value)} />
-            <label htmlFor="PrivateKey">Private Key</label>
-            <input type="text" value={Private} id="PrivateKey" onChange={(e) => setPrivate(e.target.value)} />
+            <div className="ContainerSignup">
+                <form onSubmit={Cadastrar} className="FormSingup">
+                    <div className="ContainerInputs1">
+                        <h1 >Cadastro</h1>
+                        <label htmlFor="Name">Nome Completo</label>
+                        <input type="text" value={nomeC} id="Name" onChange={(e) => setNomeC(e.target.value)} />
+                        <label htmlFor="Email">Email</label>
+                        <input type="email" value={email} id="Email" onChange={(e) => setEmail(e.target.value)} />
+                        <label htmlFor="Senha">Senha</label>
+                        <input type="password" value={senha} id="Senha" onChange={(e) => setSenha(e.target.value)} />
+                    </div>
 
-            <button type="submit">Enviar</button>
+                    <div className="ContainerInputs2">
+                        <label htmlFor="AccessKey">Access Key</label>
+                        <input type="password" value={Access} id="AccessKey" onChange={(e) => setAccess(e.target.value)} />
+                        <label htmlFor="PrivateKey" className="LabelPrivateKey">Private Key</label>
+                        <input type="password" value={Private} id="PrivateKey" onChange={(e) => setPrivate(e.target.value)} />
+                        <div className="Containerbtn">
+                            <button type="submit" className="BtnSignup">Cadastrar</button>
 
-        </form>
-            <input type="file"  id="PrivateKey" onChange={CreateBucket} />
+                        </div>
+                    </div>
+
+
+
+                </form>
+                {/* <input type="file"  id="PrivateKey" /> */}
+            </div>
         </>
     )
 }
